65527c355892a563dd11557bd9feb2e754c3e58b
Manal Albahar <97870556+ManalKhAlbahar@users.noreply.github.com>
2022-03-09 22:53:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const server = require('./src/server');
 const {db} = require('./src/auth/models/index.js')
 
     db.sync().then(()=>{
-    server.start( 3008);})
+    server.start( process.env.PORT ||3008);})
 .catch(console.error) 
 
-//process.env.PORT ||
